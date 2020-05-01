19d9c46214dbbe3e11a9d9f0e5a912a77d5856c6
未名。 <unknowntokyo@users.noreply.github.com>
2020-05-01 22:10:31 +0800

    Update location-with-script.js

--- location-with-script.js
+++ location-with-script.js
@@ -6,9 +6,10 @@ if ($response.statusCode != 200) {
 
 var body = $response.body;
 var obj = JSON.parse(body);
+var title = obj['country'];
 var subtitle = obj['isp'];
 var ip = obj['query'];
 var description = obj['isp'] + '\n' + obj['ipType'];
 
 
-$done({subtitle, ip, description});
+$done({title, subtitle, ip, description});
